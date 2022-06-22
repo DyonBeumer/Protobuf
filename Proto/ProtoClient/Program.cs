b2bd92f7fb5a18fcaf429c071b80bb06f4ba7463
Dyon Beumer <Dyonunic@gmail.com>
2022-06-22 21:48:48 +0200

    Protobuf sample

---
+++ Proto/ProtoClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using ProtoClient;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "ProtoClient" });
+Console.WriteLine("HelloRequest Response message: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
